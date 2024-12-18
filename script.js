da870d277a53616080bc2627258d4d26b72fed21
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 17:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function stringChop(str, size) {
   // your code here
+	let len=str.length;
+	let upperlimit=size;
+	let lowerlimit=0;
+	let arr=[];
+	if(len<upperlimit)
+	arr.push(str);
+	while(len>upperlimit)
+		{
+			arr.push(str.substring(lowerlimit,upperlimit));
+			lowerlimit=upperlimit;
+			upperlimit=upperlimit+size;
+			
+		}
+	return arr;
 }
 
+
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
